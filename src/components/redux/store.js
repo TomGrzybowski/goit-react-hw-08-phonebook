7d2fad4e8b1c26df626cd684d5fb498da7893592
TomekGrzybek <t.grzybowski94@gmail.com>
2023-05-21 21:25:48 +0200

    bugfix

--- src/components/redux/store.js
+++ src/components/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import persistReducer from 'redux-persist/es/persistReducer';
 import storage from 'redux-persist/lib/storage';
 
